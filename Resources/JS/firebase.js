1c538253d0cf877cffabbe7b203f173b4ec76989
Aarushi Vohra <aarushivohra01@gmail.com>
2020-04-16 16:16:49 +0530

    first commit

---
+++ Resources/JS/firebase.js
@@ -0,0 +1,17 @@
+
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCZAu8eqOe5YuUBxpOqOhn7xJu9pzQ8YX4",
+    authDomain: "leave-ms.firebaseapp.com",
+    databaseURL: "https://leave-ms.firebaseio.com",
+    projectId: "leave-ms",
+    storageBucket: "leave-ms.appspot.com",
+    messagingSenderId: "506915659536",
+    appId: "1:506915659536:web:a82cd03b43301064670f95",
+    measurementId: "G-H95P45Y6DG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
